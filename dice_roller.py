c234a02e940b2f06cc8f446e354bb8cf647b1783
Jay Cole <jayccole@gmail.com>
2022-10-27 15:30:06 -0600

    adding files

---
+++ dice_roller.py
@@ -0,0 +1,10 @@
+import random
+
+print('Welcome to the Dice Roller!')
+#Want it to roll when user presses enter
+input("Press enter to continue...")
+
+dice = random.randrange(1,7)
+
+#display the dice roll
+print(dice)
